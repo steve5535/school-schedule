ffe752d0a45a6a3b02c4db63827cf130b8cb3e25
성선혁 <steve5535@naver.com>
2025-10-18 00:28:22 +0900

    feat: 시간표 탭(Frame) 추가

--- school_schedule.py
+++ school_schedule.py
@@ -1,10 +1,18 @@
 import tkinter as tk # tkinter라이브러리를 tk로 불러옴
+from tkinter import ttk # ttk모듈 불러옴
 
+# 상수 설정
 WINDOW_WIDTH = 600 # 창 가로 길이
 WINDOW_HEIGHT = 400 # 창 세로 길이
 
+# Tkinter 기본 설정
 root = tk.Tk() # 메인 창을 생성
 root.title("학교 일정 관리 앱") # 창 이름
 root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}") # 창 크기 설정
 
+notebook = ttk.Notebook(root) # root 창 안에 Notebook(탭 컨테이너) 생성
+notebook.pack(expand=True, fill='both') # 창 크기에 맞게 자동 확장
+
+tab_timetable = ttk.Frame(notebook) # 시간표 탭용 프레임 생성
+notebook.add(tab_timetable, text="시간표") # 탭에 프레임 연결,이름 지정
 root.mainloop() # 메인 루프
